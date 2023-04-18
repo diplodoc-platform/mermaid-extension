dd2166ba5ae1c03138d7bfb5aca2e2355b50eec0
3y3k0 <3y3k0@yandex-team.ru>
2023-04-18 22:39:50 +0300

    chore: Add interactive example

---
+++ example/Readme.md
@@ -0,0 +1,19 @@
+# Simple example
+
+```
+npm i
+npm start
+```
+
+Then this documentation will be rendered by [@diplodoc/transform](https://github.com/diplodoc-platform/transform) and opened in your default browser.
+
+## Diagram
+
+```mermaid
+graph TD
+    A[Christmas] -->|Get money| B(Go shopping)
+    B --> C{Let me think}
+    C -->|One| D[Laptop]
+    C -->|Two| E[iPhone]
+    C -->|Three| F[Car]
+```
---
+++ example/index.js
@@ -0,0 +1,39 @@
+import transform from '@doc-tools/transform';
+import mermaid from '@diplodoc/mermaid-extension';
+import { readFile } from 'node:fs/promises'
+
+(async () => {
+    const content = await readFile('./Readme.md', 'utf8');
+    const {result} = await transform(content, {
+        output: './build',
+        plugins: [
+            mermaid.transform()
+        ]
+    });
+
+    const scripts = result.meta.script.map((script) => {
+        return `<script src="${script}"></script>`;
+    }).join('\n');
+
+    const html = `
+<html>
+    <head>
+        ${scripts}
+        <script>
+            window.mermaidJsonp = window.mermaidJsonp || [];
+            window.mermaidJsonp.push(function(mermaid) {
+                window.addEventListener('load', function() {
+                    mermaid.initialize({ theme: 'forest' });                    
+                    mermaid.run();
+                });
+            });
+        </script>
+    </head>
+    <body style="background: #FFF">
+        ${result.html}    
+    </body>
+</html>    
+    `
+
+    console.log(html);
+})();
