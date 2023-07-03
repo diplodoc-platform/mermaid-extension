411fd84450dbf8570404c523e471bc5a01e25dc8
smsochneg <cjmixer2@gmail.com>
2023-07-03 11:05:57 +0200

    feat: added render and error methods to mermaid instance (#4)

--- src/@types/common.d.ts
+++ src/@types/common.d.ts
@@ -5,6 +5,10 @@ type RunOptions = Pick<MermaidRunOptions, 'querySelector' | 'nodes'>;
 type Callback = (exposed: {
     run: (options?: RunOptions) => Promise<void>;
     initialize: typeof mermaid.initialize;
+    render: typeof mermaid.render;
+    parseError: typeof mermaid.parseError,
+    parse: typeof mermaid.parse,
+    setParseErrorHandler: typeof mermaid.setParseErrorHandler
 }) => void;
 
 declare global {
