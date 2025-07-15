8cbad54b18612c32c22c7d2716869749d6ed0ca2
Anton Vikulov <rndnm@yandex-team.ru>
2025-07-15 09:31:51 +0300

    feat(nonce): add nonce attr

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import type mermaid from 'mermaid';
 // eslint-disable-next-line no-duplicate-imports
 import type {MermaidConfig, RunOptions as MermaidRunOptions} from 'mermaid';
 
-export type RunOptions = Pick<MermaidRunOptions, 'querySelector' | 'nodes'>;
+export type RunOptions = Pick<MermaidRunOptions, 'querySelector' | 'nodes'> & {nonce?: string};
 
 export type InitConfig = MermaidConfig & {
     /**
